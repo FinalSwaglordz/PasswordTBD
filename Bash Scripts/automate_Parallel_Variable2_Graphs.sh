1da7b181b79289fbd26b6795873934ecb12f1353
Damon Hage <damon.hage@live.com>
2017-05-09 14:36:21 -0500

    yep

---
+++ Bash Scripts/automate_Parallel_Variable2_Graphs.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+./Executables/Parallel_Variable2 zzzzzz 1;
+./Executables/Parallel_Variable2 zzzzzz 2;
+./Executables/Parallel_Variable2 zzzzzz 3;
+./Executables/Parallel_Variable2 zzzzzz 4;
+./Executables/Parallel_Variable2 zzzzzz 5;
+./Executables/Parallel_Variable2 zzzzzz 6;
+./Executables/Parallel_Variable2 zzzzzz 7;
+./Executables/Parallel_Variable2 zzzzzz 8;
+./Executables/Parallel_Variable2 zzzzzz 9;
+./Executables/Parallel_Variable2 zzzzzz 10;
+./Executables/Parallel_Variable2 zzzzzz 16;
+./Executables/Parallel_Variable2 zzzzzz 26;
